c5127b331a73e5b7433fe4d15beea169ed8c7911
Viktor Bocz-Kemeny <beviktor95@gmail.com>
2023-09-05 16:01:47 +0200

    Fix response handling delivered by backend

--- src/services/SecretService.js
+++ src/services/SecretService.js
@@ -1,45 +1,62 @@
 import axios from "axios";
 import { getBaseUrl, handleError, getResponseMessage } from "./CommonService";
 
-const secretUrl = `${ getBaseUrl() }/secrets`;
+const secretUrl = `${getBaseUrl()}/secrets`;
 
-const sendDeleteSecretRequest = (keyVaultName, secretRegex, callbackForLoading, callbackForDataSaving, callbackForOnDelete) => {
-    callbackForLoading(true);
-    let url = `${secretUrl}/deletesecret`;
-    let body = {
-      "keyVaultName": keyVaultName,
-      "secretFilter": secretRegex
-    }
-    axios.post(url, body)
-      .then(res => {
-        callbackForDataSaving(res.data.secrets);
-        callbackForLoading(false)
-        callbackForOnDelete(false)
-        alert(getResponseMessage(res.data.status))
-      })
-      .catch(err => {
-        handleError(callbackForLoading, err);
-      });
-  }
-  
-  const sendListSecretRequest = (keyVaultName, secretRegex, callbackForDataSaving, callbackForLoading) => {
-    let url = `${secretUrl}/getsecrets?keyVaultName=${keyVaultName}&secretFilter=${secretRegex}`;
-    callbackForLoading(true);
-    axios.get(url)
-      .then(res => {
-        let status = res.data.status;
-        callbackForDataSaving(res.data.secrets);
-        callbackForLoading(false);
-        if (status !== 0) {
-          alert(getResponseMessage(res.data.status));
-        }
-      })
-      .catch(err => {
-        handleError(callbackForLoading, err);
-      });
-  }
-
-  export {
-    sendDeleteSecretRequest,
-    sendListSecretRequest
+const sendDeleteSecretRequest = (
+  keyVaultName,
+  secretRegex,
+  callbackForLoading,
+  callbackForDataSaving,
+  callbackForOnDelete
+) => {
+  callbackForLoading(true);
+  let url = `${secretUrl}/deletesecret`;
+  let body = {
+    keyVaultName: keyVaultName,
+    secretFilter: secretRegex,
+  };
+  axios
+    .post(url, body)
+    .then((res) => {
+      let status = res.data.status;
+      let secrets = res.data.secrets;
+      callbackForLoading(false);
+      callbackForOnDelete(false);
+      if (status === 0) {
+        callbackForDataSaving(secrets);
+      } else {
+        alert(getResponseMessage(status));
+      }
+    })
+    .catch((err) => {
+      handleError(callbackForLoading, err);
+    });
 };
+
+const sendListSecretRequest = (
+  keyVaultName,
+  secretRegex,
+  callbackForDataSaving,
+  callbackForLoading
+) => {
+  let url = `${secretUrl}/getsecrets?keyVaultName=${keyVaultName}&secretFilter=${secretRegex}`;
+  callbackForLoading(true);
+  axios
+    .get(url)
+    .then((res) => {
+      let status = res.data.status;
+      let secrets = res.data.secrets;
+      callbackForLoading(false);
+      if (status === 0) {
+        callbackForDataSaving(secrets);
+      } else {
+        alert(getResponseMessage(status));
+      }
+    })
+    .catch((err) => {
+      handleError(callbackForLoading, err);
+    });
+};
+
+export { sendDeleteSecretRequest, sendListSecretRequest };
