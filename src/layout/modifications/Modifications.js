ece07ef06f94094d188b4261d8107743d580a081
Viktor Bocz-Kemeny <beviktor95@gmail.com>
2023-11-18 18:23:18 +0100

    Added functionality to go back to main

--- src/layout/modifications/Modifications.js
+++ src/layout/modifications/Modifications.js
@@ -1,12 +1,22 @@
 import React from "react";
 import { ModificationsForm } from "./Forms/ModificationsForm";
 import { ModificationsTable } from "./Results/ModificationsTable";
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 export const Modifications = () => {
+  const navigate = useNavigate();
   return (
     <>
+      <Button
+        variant="contained"
+        id="back_to_main"
+        onClick={() => navigate("/")}
+      >
+        Back to main
+      </Button>
       <ModificationsForm />
-      <ModificationsTable/>
+      <ModificationsTable />
     </>
   );
 };
