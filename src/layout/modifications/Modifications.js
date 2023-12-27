f97a8c567a84b76d2f070ea80554c60f9d5fb8a6
Viktor Bocz-Kemeny <beviktor95@gmail.com>
2023-12-27 12:34:50 +0100

    Empty changes table

--- src/layout/modifications/Modifications.js
+++ src/layout/modifications/Modifications.js
@@ -1,17 +1,22 @@
-import React from "react";
+import React, {useContext} from "react";
 import { ModificationsForm } from "./Forms/ModificationsForm";
 import { ModificationsTable } from "./Results/ModificationsTable";
 import { Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import { ChangesContext } from "../../contexts/Contexts";
 
 export const Modifications = () => {
+  const { setChanges } = useContext(ChangesContext);
   const navigate = useNavigate();
   return (
     <>
       <Button
         variant="contained"
         id="back_to_main"
-        onClick={() => navigate("/")}
+        onClick={() => {
+          setChanges([]);
+          navigate("/");
+        }}
       >
         Back to main
       </Button>
