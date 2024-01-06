04f6629d78d7c2e1719bc9668bbb528a68f6ae65
Viktor Bocz-Kemeny <beviktor95@gmail.com>
2024-01-06 13:28:05 +0100

    Outsourced rows to different components from sync table

--- src/layout/main/Results/Sync/SyncTableBody.js
+++ src/layout/main/Results/Sync/SyncTableBody.js
@@ -1,152 +1,66 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect } from "react";
 import { v4 } from "uuid";
 import {
   ContainingVGsContext,
-  ContainingVGsProjectContext,
-  OrganizationContext,
-  PATContext,
   PaginationCounterContext,
-  ProfileNameContext,
+  PipelineConnectedVGsContext,
   VariablesSyncContext,
 } from "../../../../contexts/Contexts";
-import ContainingVGSelectMenu from "./ContainingVGSelectMenu";
-import { AiFillEdit, AiOutlineCheck, AiOutlineClose } from "react-icons/ai";
-import { syncVariableGroup } from "../../../../services/VariableGroupServices/VariableGroupService";
-import SyncTableBodyInput from "./SyncTableBodyInput";
-import { toastErrorPopUp } from "../../../../services/CommonService";
+import SyncTableBodyRow from "./SyncTableBodyRow";
 
 const SyncTableBody = () => {
   const { paginationCounter } = useContext(PaginationCounterContext);
-  const { containingVGs, setContainingVGs } = useContext(ContainingVGsContext);
-  const { syncVariables, setSyncVariables } = useContext(VariablesSyncContext);
-  const { pat } = useContext(PATContext);
-  const { organizationName } = useContext(OrganizationContext);
-  const { profileName } = useContext(ProfileNameContext);
+  const { containingVGs } = useContext(ContainingVGsContext);
+  const { syncVariables } = useContext(VariablesSyncContext);
+  const { pipelineConnectedVGs } = useContext(PipelineConnectedVGsContext);
 
-  const { containingVGsProject } = useContext(ContainingVGsProjectContext);
-
-  const [modification, setModification] = useState({});
-  const [localLoading, setLocalLoading] = useState(false);
-  const [newVariableKey, setNewVariableKey] = useState("");
   const number = 10;
 
-  const changeSync = async (variableToBeReplaced) => {
-    let newKey = document.getElementById(
-      `inline-var-${variableToBeReplaced}`
-    ).value;
-    if (syncVariables.includes(newKey)) {
-      toastErrorPopUp(
-        "New key is already in variables list!",
-        "key-update",
-        1500
-      );
-    } else {
-      setNewVariableKey(newKey);
-      let newSyncVariables = collectNewSyncVariables(
-        variableToBeReplaced,
-        newKey
-      );
-      setSyncVariables(newSyncVariables);
-      let [indexOfVarToBeReplaced, newContainingVGs] =
-        collectNewContainingVGs(variableToBeReplaced);
-      let body = {
-        projectName: containingVGsProject,
-        pat: pat,
-        userName: profileName,
-        vgRegex: ".*",
-        keyRegex: newKey,
-        organizationName: organizationName,
-        secretIncluded: true,
-        containsKey: true,
-      };
-      await syncVariableGroup(
-        indexOfVarToBeReplaced,
-        body,
-        newContainingVGs,
-        setContainingVGs,
-        setLocalLoading
-      );
-    }
-    setModification({});
-  };
+  useEffect(() => {
+    console.log(containingVGs);
+  }, [containingVGs]);
 
-  const collectNewContainingVGs = (variableToBeReplaced) => {
-    let newContainingVGs = [];
-    let indexOfVarToBeReplaced = 0;
-    containingVGs.forEach((vgElement) => {
-      if (vgElement.key !== variableToBeReplaced) {
-        newContainingVGs.push(vgElement);
-      } else {
-        indexOfVarToBeReplaced = vgElement.index;
+  const collectPotentialMissingVgs = (variableType, vgsNames) => {
+    let potentialMissingVgs = [];
+    pipelineConnectedVGs.forEach((element) => {
+      if (
+        (element.Type === variableType || variableType === "Unknown") &&
+        !vgsNames.includes(element.variableGroupName)
+      ) {
+        potentialMissingVgs.push(element);
       }
     });
-    return [indexOfVarToBeReplaced, newContainingVGs];
+    return potentialMissingVgs;
   };
 
-  const collectNewSyncVariables = (variableToBeReplaced, newKey) => {
-    let newSyncVariables = [];
-    syncVariables.forEach((element) => {
-      if (element === variableToBeReplaced) {
-        newSyncVariables.push(newKey);
-      } else {
-        newSyncVariables.push(element);
-      }
-    });
-    return newSyncVariables;
-  };
-
-  const getApprovalButtons = (variable) => {
-    return (
-      <>
-        <button onClick={async () => await changeSync(variable)}>
-          <AiOutlineCheck />
-        </button>
-        <button onClick={() => setModification({})}>
-          <AiOutlineClose />
-        </button>
-      </>
-    );
-  };
-
-  const getEditButton = (variable) => {
-    return (
-      <abbr title={"Custom modification of variable's key"}>
-        <button
-          onClick={() =>
-            setModification({
-              variable: variable,
-              modification: true,
-            })
+  const collectVgs = (variableKey, pipelineConnectedVGsNames) => {
+    let vgs = [];
+    let vgsNames = [];
+    let variableType = "";
+    containingVGs.forEach((element) => {
+      if (element.key === variableKey) {
+        variableType = element.variableGroupType;
+        element.result.forEach((element) => {
+          if (pipelineConnectedVGsNames.includes(element.variableGroupName)) {
+            vgs.push(element);
+            vgsNames.push(element.Name);
           }
-        >
-          <AiFillEdit />
-        </button>
-      </abbr>
-    );
+        });
+      }
+    });
+    return {
+      vgs: vgs,
+      vgsNames: vgsNames,
+      variableType: variableType,
+    };
   };
 
-  const getActionSection = (variable) => {
-    return localLoading && variable === newVariableKey ? (
-      <span>Loading...</span>
-    ) : (
-      getActionButtons(variable)
-    );
-  };
-
-  const getActionButtons = (variable) => {
-    return modification.modification && modification.variable === variable
-      ? getApprovalButtons(variable)
-      : getEditButton(variable);
-  };
-
-  const getVariableKey = (variable) => {
-    return modification.modification && modification.variable === variable ? (
-      <SyncTableBodyInput variable={variable} />
-    ) : (
-      <>
-        {variable}
-      </>
-    );
+  const collectPipelineConnectedVGsNames = () => {
+    let pipelineConnectedVGsNames = [];
+    pipelineConnectedVGs.forEach((element) => {
+      pipelineConnectedVGsNames.push(element.Name);
+    });
+    return pipelineConnectedVGsNames;
   };
 
   return (
@@ -154,44 +68,23 @@ const SyncTableBody = () => {
       {syncVariables
         .slice(paginationCounter, paginationCounter + number)
         .map((variable) => {
-          let variableType = "";
-          containingVGs.forEach((element) => {
-            if (element.key === variable) {
-              variableType = element.variableGroupType;
-            }
-          });
+          let pipelineConnectedVGsNames = collectPipelineConnectedVGsNames();
+          let resultObj = collectVgs(variable, pipelineConnectedVGsNames);
+          let variableType = resultObj["variableType"];
+          let vgs = resultObj["vgs"];
+          let vgsNames = resultObj["vgsNames"];
+          let potentialMissingVgs = collectPotentialMissingVgs(
+            variableType,
+            vgsNames
+          );
           return (
-            <tr key={v4()}>
-              <td key={v4()}>
-                {localLoading && variable === newVariableKey ? (
-                  <>{newVariableKey}</>
-                ) : (
-                  getVariableKey(variable)
-                )}
-              </td>
-              <td key={v4()}>
-              {containingVGsProject === "" ? (
-                  <>-</>
-                ) : (
-                  <>{variableType === "AzureKeyVault"? "Secret": variableType === "Vsts"? "Standard variable": "Unknown"}</>
-                )}
-              </td>
-              <td key={v4()}>
-                {containingVGsProject === "" ? (
-                  <>-</>
-                ) : (
-                  getActionSection(variable)
-                )}
-              </td>
-              <td key={v4()}>
-                {containingVGsProject === "" ||
-                (localLoading && variable === newVariableKey) ? (
-                  <p>-</p>
-                ) : (
-                  <ContainingVGSelectMenu variableName={variable} />
-                )}
-              </td>
-            </tr>
+            <SyncTableBodyRow
+              key={v4()}
+              vgs={vgs}
+              potentialMissingVgs={potentialMissingVgs}
+              variable={variable}
+              variableType={variableType}
+            />
           );
         })}
     </tbody>
