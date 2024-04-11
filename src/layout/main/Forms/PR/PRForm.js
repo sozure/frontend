42e061f14bbeb603a1d3206c648fdeaabc1a40af
Viktor Bocz-Kemeny <beviktor95@gmail.com>
2024-04-11 13:15:37 +0200

    Added refresh button to form

--- src/layout/main/Forms/PR/PRForm.js
+++ src/layout/main/Forms/PR/PRForm.js
@@ -1,45 +1,56 @@
-import React, { useContext } from 'react'
-import { LoadingContext, OrganizationContext, PATContext, ProjectNameContext, PullRequestsContext } from '../../../../contexts/Contexts';
-import ProjectSelectMenu from '../../../ProjectSelectMenu';
-import MatUIButton from '../../../MatUIButton';
-import { getPullRequests } from '../../../../services/GitPullRequestService';
+import React, { useContext, useState } from "react";
+import {
+  LoadingContext,
+  OrganizationContext,
+  PATContext,
+  ProjectNameContext,
+  PullRequestsContext,
+} from "../../../../contexts/Contexts";
+import ProjectSelectMenu from "../../../ProjectSelectMenu";
+import MatUIButton from "../../../MatUIButton";
+import { getPullRequests } from "../../../../services/GitPullRequestService";
+import RefreshIcon from "@mui/icons-material/Refresh";
 
 const PRForm = () => {
-    const { projectName, setProjectName } = useContext(ProjectNameContext);
-    const { organizationName } = useContext(OrganizationContext);
-    const { pat } = useContext(PATContext);
-    const { setLoading } = useContext(LoadingContext);
-    const { setPullRequests } = useContext(PullRequestsContext);
+  const { projectName, setProjectName } = useContext(ProjectNameContext);
+  const { organizationName } = useContext(OrganizationContext);
+  const { pat } = useContext(PATContext);
+  const { setLoading } = useContext(LoadingContext);
+  const { setPullRequests } = useContext(PullRequestsContext);
 
-    const customSetProject = (value) => {
-        setProjectName(value);
-    };
+  const [isRefresh, setIsRefresh] = useState(false);
 
-    const send = async () => {
-      setLoading(true);
-      let projectToBeAdded = projectName === "All" ? null : projectName;
-      let basicData = {
-        organization: organizationName,
-        pat: pat,
-        project: projectToBeAdded,
-      }
-      await getPullRequests(basicData, setLoading, setPullRequests);
+  const customSetProject = (value) => {
+    setProjectName(value);
+    setIsRefresh(false);
+  };
+
+  const send = async () => {
+    setLoading(true);
+    let projectToBeAdded = projectName === "All" ? null : projectName;
+    let basicData = {
+      organization: organizationName,
+      pat: pat,
+      project: projectToBeAdded,
     };
+    await getPullRequests(basicData, setLoading, setPullRequests);
+    setIsRefresh(true);
+  };
 
   return (
     <div className="form">
-        <ProjectSelectMenu
-          allOption={true}
-          projectName={projectName}
-          setProjectName={customSetProject}
-        />
-        <MatUIButton
+      <ProjectSelectMenu
+        allOption={true}
+        projectName={projectName}
+        setProjectName={customSetProject}
+      />
+      <MatUIButton
         id={"request_pull_requests"}
         send={send}
-        displayName={"Get pull requests"}
+        displayName={isRefresh ? <RefreshIcon /> : "Get pull requests"}
       />
     </div>
-  )
-}
+  );
+};
 
-export default PRForm
+export default PRForm;
