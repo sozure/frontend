09ec96d1f745f3d08f8763f53ff038ed42a3a489
Viktor Bocz-Kemeny <beviktor95@gmail.com>
2024-04-24 17:29:13 +0200

    Small fixes of PR listing

--- src/layout/main/Results/PR/PRTableBodyRow.js
+++ src/layout/main/Results/PR/PRTableBodyRow.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { v4 } from "uuid";
 import { Box, Button } from "@mui/material";
 import NearMeIcon from "@mui/icons-material/NearMe";
@@ -7,10 +7,19 @@ import MatUISelect from "../../../MatUISelect";
 
 const PRTableBodyRow = ({ pullRequest }) => {
   const [approver, setApprover] = useState("");
+  const [approvers, setApprovers] = useState([]);
 
   const navigate = (url) => {
     window.open(url, "_blank");
   };
+
+  useEffect(() => {
+    let result = "";
+    pullRequest.approvers.forEach((approver) => {
+      result += `${approver}, `;
+    });
+    setApprovers(result.slice(0, -2));
+  }, [setApprovers, pullRequest]);
 
   return (
     <tr key={v4()}>
@@ -25,7 +34,10 @@ const PRTableBodyRow = ({ pullRequest }) => {
       <td key={v4()}>{pullRequest.created}</td>
       <td key={v4()}>{pullRequest.size[0]}</td>
       <td key={v4()}>
-        {pullRequest.approvers.length > 0 ? (
+        {pullRequest.approvers.length > 0 && pullRequest.approvers.length < 3 && (
+          <>{approvers}</>
+        )}
+        {pullRequest.approvers.length > 2 && (
           <MatUISelect
             collection={pullRequest.approvers}
             inputLabel={"Approvers"}
@@ -34,9 +46,8 @@ const PRTableBodyRow = ({ pullRequest }) => {
             setSelectValue={setApprover}
             allOption={false}
           />
-        ) : (
-          <>No approvers</>
         )}
+        {pullRequest.approvers.length === 0 && <>-</>}
       </td>
       <td key={v4()}>
         <Box>
